e9a970bb7774874fad975c83ef22c8b0903304a9
Shaniagrg <sglearner2022@gmail.com>
2026-09-04 15:59:25 -0700

    add comment on when while loop executes

--- loop/while_loop.js
+++ loop/while_loop.js
@@ -3,6 +3,8 @@ function getRandomNumber(upper) {
   }
   
   let counter = 0;
+
+  //It will 1st check the condition then move to line 9 if the condition is true
   while ( counter < 10 ) {
     console.log( getRandomNumber(10) );
     counter += 1;
