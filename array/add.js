9a14dd13a28021e3851ef790f0f9cdc42ab2327b
Shaniagrg <sglearner2022@gmail.com>
2026-09-05 22:23:17 -0700

    Add element at the end and beginning of an array using push() and unshift()

---
+++ array/add.js
@@ -0,0 +1,18 @@
+const instruments = ['piano', 'drums', 'trumpet'];
+
+//push adds elements at the END of an array
+instruments.push('guitar');
+console.log(instruments);
+
+//You can add more than one element in an array
+instruments.push('guitar', 'flute');
+console.log(instruments);
+
+
+//unshift adds elements at the BEGINNING of an array
+instruments.unshift('bell');
+console.log(instruments);
+
+//You can add more than one element in an array
+instruments.unshift('bell','flute');
+console.log(instruments);
