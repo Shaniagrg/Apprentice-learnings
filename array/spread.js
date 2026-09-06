5f8634b0095529cf668cd61a03003d42270f42f1
Shaniagrg <sglearner2022@gmail.com>
2026-09-05 22:53:31 -0700

    adding an element to a cup variable won't show on variable starbucks because the value in starbucks were copied before adding. therefore adding/removing value after making copy won't affect to the variable added

--- array/spread.js
+++ array/spread.js
@@ -11,3 +11,10 @@ const drink = ['cappucino', 'latte'];
 const starbucks = [...cup, ...drink];
 
 console.log(starbucks);
+
+//add an element in the cup
+cup.push('grande');
+console.log(cup);
+
+//You won't see the value added inside cup added to starbucks cuz the value is already copied
+console.log(starbucks);
