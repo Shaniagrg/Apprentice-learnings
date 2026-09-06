389412cb05ef54e17ce62af2465c08790e022748
Shaniagrg <sglearner2022@gmail.com>
2026-09-05 22:25:17 -0700

    Remove elements in an array at end by using pop() and beginning using shift()

---
+++ array/remove.js
@@ -0,0 +1,11 @@
+const numbers = [100, 200, 300, 400, 500]
+
+//Pop removes elements from the END
+numbers.pop()
+console.log(numbers);
+
+//Shift removes element from the beginning of an array 
+//Also you can simply store the poped value inside an variable
+const firstNumbers = numbers.shift()
+console.log(numbers);
+console.log(firstNumbers);
