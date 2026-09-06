36e8d997455c70366b9d0c629691aee971112641
Shaniagrg <sglearner2022@gmail.com>
2026-09-06 10:49:21 -0700

    comment an example with if conditionals and use lowercase for includes()

--- array/include.js
+++ array/include.js
@@ -6,3 +6,19 @@ console.log(week.includes('Monday'));
 //check if Week included 'Tommy' and it does NOT so Prints false
 console.log(week.includes('Tommy'));
 
+/*
+const inStock = ['pizza', 'cookies', 'eggs', 'apples', 'milk', 'cheese', 'bread', 'lettuce', 'carrots', 'broccoli', 'potatoes', 'crackers', 'onions', 'tofu', 'limes', 'cucumbers'];
+const inStock = ['pizza', 'cookies', 'eggs', 'apples', 'milk', 'cheese', 'bread', 'lettuce', 'carrots', 'broccoli', 'potatoes', 'crackers', 'onions', 'tofu', 'limes', 'cucumbers'];
+const search = prompt('Search for a product.');
+let message;
+
+if (!search){
+  message = `<strong>In stock:</strong>${inStock.join(', ')}`;
+}else if (inStock.includes(search.toLowerCase())){
+  message = `Yes, we have <strong>${search}</strong>`;
+}else{
+  message = `Sorry we don't have <strong>${search}</strong>. It's #${inStock.indexOf(search.toLowerCase())}`;
+}
+
+document.querySelector('main').innerHTML = `<p>${message}</p>`;
+*/
