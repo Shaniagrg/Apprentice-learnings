6992ca9253522a98d793216636cd7c755f46f303
Shaniagrg <sglearner2022@gmail.com>
2026-09-05 22:42:18 -0700

    use spread operator in an array to combine 2 or more array anywhere inside an array

---
+++ array/spread.js
@@ -0,0 +1,13 @@
+//Spread operator allows you to build, combine and manipulate arrays quickly and more seamlessly
+
+const middle = ['lettuce', 'cheese', 'patty'];
+//... the 3 dots is necessary 
+const burger = ['top bun', ...middle, 'bottom bun'];
+console.log(burger);
+
+const cup = ['small', 'medium', 'large'];
+const drink = ['cappucino', 'latte'];
+
+const starbucks = [...cup, ...drink];
+
+console.log(starbucks);
