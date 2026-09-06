2a24700d9e206d4c5981c4441a6082cca93e0c5f
Shaniagrg <sglearner2022@gmail.com>
2026-09-06 02:17:54 -0700

    Use include to find the element inside an array

---
+++ array/include.js
@@ -0,0 +1,8 @@
+const week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+//check if Week included 'Monday' and it does so Prints true
+console.log(week.includes('Monday'));
+
+//check if Week included 'Tommy' and it does NOT so Prints false
+console.log(week.includes('Tommy'));
+
