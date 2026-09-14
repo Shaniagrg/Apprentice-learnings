c27067900349844d7a30b5441d5d91444e371333
Shaniagrg <sglearner2022@gmail.com>
2026-09-14 16:57:47 -0700

    organize

--- unit2/dom/event_listen_by_ID/app.js
+++ unit2/dom/event_listen_by_ID/app.js
@@ -2,8 +2,13 @@
 document.getElementById('headline') => this selects the id name
 .style.border => Here you are trying to style the border
 
+
 code: document.getElementById('headline').style.border = 'solid 2px red'
 */
+
+
+/*
+//lETS MAKE THIS CODE AS ONE
 
 //make a habit of making a variable and store the document. so that the code doesnt look messy
 
@@ -13,3 +18,14 @@ headline.style.border = 'solid 2px red'
 
 const btn_main = document.getElementById('btn-main');
 btn_main.style.border = 'dotted 3px lavender' 
+*/
+
+
+const headline = document.getElementById('headline');
+const btn_main = document.getElementById('btn-main');
+btn_main.style.border = 'dotted 3px lavender' 
+
+btn_main.addEventListener('click', () => {
+    headline.style.border = 'solid 2px red';
+    headline.style.fontSize = '20px'
+});
