b744afc74caa5e029e0916e3b821503704584be1
Shaniagrg <sglearner2022@gmail.com>
2026-09-14 18:27:23 -0700

    select elements by class name

---
+++ unit2/dom/by_class_name/app.js
@@ -0,0 +1,11 @@
+/*
+Use for of loop
+Give only 3 <li> a class name and change the background color
+*/
+
+const highlights = document.getElementsByClassName('highlight');
+
+for (const i of highlights){
+  i.style.backgroundColor = "cornsilk"
+}
+   
